887a108492a84eb0ea9c9972ff7e7ee88e9e3f75
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-08-22 23:14:49 -0500

    Adding books REST API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './controllers/books/books.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [BooksModule],
 })
 export class AppModule {}
