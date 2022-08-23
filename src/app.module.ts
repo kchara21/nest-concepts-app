4507aa91abe2d319c3ea8b5dddd88e2820e2c564
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-08-22 22:35:31 -0500

    Deleting cat controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './controllers/cats/cats.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [CatsModule],
 })
 export class AppModule {}
