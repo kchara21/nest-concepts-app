548bff327b0bc2120e019571f145fcb2769bbf23
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2022-09-03 01:51:20 -0500

    Importing configService of TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configService } from './config/config.service';
 import { BooksModule } from './controllers/books/books.module';
 
 @Module({
+  imports: [
+    BooksModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [BooksModule],
 })
 export class AppModule {}
